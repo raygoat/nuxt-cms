179e22c9a12077b1f5ca1ac2804f68267d93c12d
René Gedde <diverse@oob.dk>
2021-11-19 17:36:50 +0100

    Added pictures to projects

--- components/global/Header.vue
+++ components/global/Header.vue
@@ -2,7 +2,7 @@
   <nav class="scrim-bg fixed z-40 top-0 inset-x-0 pt-3 px-3" aria-label="Main Menu">
     <ul class="flex">
       <li class="flex-1">
-        <nuxt-link class="btn block" to="/">Home2</nuxt-link>
+        <nuxt-link class="btn block" to="/">Home</nuxt-link>
       </li>
       <li class="flex-1 ml-2">
         <nuxt-link class="btn block" to="/blog">Blog</nuxt-link>
